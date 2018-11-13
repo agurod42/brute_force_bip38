934cef2821c9955f1ffed66cf272309ef6d620df
Agustín Rodríguez <me@agurodriguez.net>
2018-11-13 01:09:14 -0300

    install dependencies when ./build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-set -x #echo on
+
+# Install dependencies
+
+echo "Installing dependencies..."
+uname="$(uname -s)"
+case "${uname}" in
+    Linux*)     sudo apt-get install -y build-essential cmake3 libtool
+                ;;
+    Darwin*)    brew install autoconf automake cmake libtool
+                ;;
+    *)          echo "Error: cannot build on ${uname}"
+                exit 1
+esac
+
+# Build libwally-core
 
 if [ ! -f third_party/libwally-core/src/config.h ]; then
+    echo "Building libwally-core..."
     cd third_party/libwally-core
     ./tools/autogen.sh
     ./configure
@@ -11,8 +26,14 @@ if [ ! -f third_party/libwally-core/src/config.h ]; then
     cd ../..
 fi
 
+# Build brute_force_bip38
+
+echo "Building brute_force_bip38..."
 mkdir -p build
 cd build
 cmake ..
 make
 cd ..
+
+# Done :)
+echo "Done."
